4fb54630fbe6db9d9157c0df3f79b04a9288c4e1
MrMessy77 <313691730@qq.com>
2017-07-04 16:18:47 +0800

    修改plugin

--- www/ezviz.js
+++ www/ezviz.js
@@ -1,36 +1,28 @@
-cordova.define("cordova-plugin-ezviz.ezviz", function(require, exports, module) {
+/*global cordova, module*/
 
-    /*global cordova, module*/
-
-    module.exports = {
+module.exports = {
         greet: function (name, successCallback, errorCallback) {
-            cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
-        },
-        init:function(accessToken){
-
-            cordova.exec(function(){},
-                function(error){
-                    console.log('list camera error:',error);
-                },
-                "ezviz",
-                "init",
-                [accessToken]);
-
-        },
-        listCamera:function(successCallback,errorCallback){
-            cordova.exec(function(){}, function(error){
+        cordova.exec(successCallback, errorCallback, "ezviz", "greet", [name]);
+    },
+    init:function(accessToken){
+        cordova.exec(function(){},
+            function(error){
                 console.log('list camera error:',error);
-            }, "ezviz", "listCamera", []);
-        },
-        preview:function(accessToken,deviceSerial,cameraIndex,eventName,doorCaption,lightCaption,successCallback,errorCallback){
-            cordova.exec(successCallback || function(){},
-                errorCallback || function(error){   console.log('open camera error:',error);},
-                "ezviz",
-                "preview",
-                [accessToken||"",deviceSerial,cameraIndex || 0,eventName,doorCaption || "",lightCaption || ""]);
-        }
-    };
-
-
-
-});
+            },
+            "ezviz",
+            "init",
+            [accessToken]);
+    },
+    listCamera:function(successCallback,errorCallback){
+        cordova.exec(function(){}, function(error){
+            console.log('list camera error:',error);
+        }, "ezviz", "listCamera", []);
+    },
+    preview:function(accessToken,deviceSerial,cameraIndex,eventName,doorCaption,lightCaption,successCallback,errorCallback){
+        cordova.exec(successCallback || function(){},
+            errorCallback || function(error){   console.log('open camera error:',error);},
+            "ezviz",
+            "preview",
+            [accessToken||"",deviceSerial,cameraIndex || 0,eventName || "",doorCaption || "",lightCaption || ""]);
+    }
+};
